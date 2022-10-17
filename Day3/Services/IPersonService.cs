69d1c20128d9baa0e71e9368a3da8f028f98550c
NguyenThanhLoc <87875758+Paimonmorningstar@users.noreply.github.com>
2022-10-17 11:42:22 +0700

    day3

---
+++ Day3/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+using Day3.Models;
+
+namespace Day3.Services
+{
+    public interface IPersonService
+    {
+        List<PersonModel> GetAll();
+        PersonModel? GetOne(int index);
+        PersonModel Create(PersonModel model);
+        PersonModel? Update(int index, PersonModel model);
+        PersonModel? Delete(int index);
+    }
+}
---
+++ Day3/Services/PersonService.cs
@@ -0,0 +1,80 @@
+using Day3.Models;
+
+namespace Day3.Services
+{
+    public class PersonService : IPersonService
+    {
+        private static List<PersonModel> _people = new List<PersonModel>{
+            new PersonModel{
+                FirstName = "Loc",
+                LastName  = "Nguyen Thanh",
+                Gender = "Male",
+                DateOfBirth = new DateTime(2000,01,21),
+                PhoneNumber="0961564730",
+                BirthPlace = "Bac Ninh",
+                IsGraduated = false
+            },
+            new PersonModel{
+                FirstName = "Hung",
+                LastName  = "Vu Trong",
+                Gender = "Male",
+                DateOfBirth = new DateTime(2000,07,06),
+                PhoneNumber="00223344885",
+                BirthPlace = "Lao Cai",
+                IsGraduated = false
+            },
+            new PersonModel{
+                FirstName = "Linh",
+                LastName  = "Nguyen Nhat",
+                Gender = "Female",
+                DateOfBirth = new DateTime(2000,07,24),
+                PhoneNumber="21354354885",
+                BirthPlace = "Ha Noi",
+                IsGraduated = false
+            },
+        };
+        List<PersonModel> IPersonService.GetAll()
+        {
+            return _people;
+        }
+
+        public PersonModel? GetOne(int index)
+        {
+            if (index >= 0 && index < _people.Count)
+            {
+                return _people[index];
+            }
+
+            return null;
+        }
+
+        public PersonModel Create(PersonModel model)
+        {
+            _people.Add(model);
+            return model;
+        }
+
+        public PersonModel? Update(int index, PersonModel model)
+        {
+            if (index >= 0 && index < _people.Count)
+            {
+                _people[index]=model;
+                return model;
+            }
+
+            return null;
+        }
+
+        public PersonModel? Delete(int index)
+        {
+            if (index >= 0 && index < _people.Count)
+            {
+                var person = _people[index];
+                _people.RemoveAt(index);
+                return person;
+            }
+
+            return null;
+        }
+    }
+}
